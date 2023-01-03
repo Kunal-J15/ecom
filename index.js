193533db2edae210aa950779c91c634bb06cbdc9
KJ <subodhsjagtap19@gmail.com>
2023-01-03 15:15:34 +0530

    backend API implement

--- index.js
+++ index.js
@@ -3,17 +3,44 @@ const sideBar =[];
 let msg;
 let total=0;
 const totalDiv =document.querySelector("h4");
-console.log(totalDiv);
+
 const pushDiv = document.getElementsByClassName("fixed-bottom")[0];
 const sideDiv = document.getElementById("sideBar");
 const sideClosebtn = document.querySelector(".sideBar button");
+sideClosebtn.addEventListener("click",close);
 const cartBtn= document.getElementsByClassName("btn btn-outline-success position-absolute top-0 end-0")[0];
+cartBtn.addEventListener("click",showCart)
+const mainProductDiv = document.querySelector(".row, .align-items-start");
+loadProducts();
+async function loadProducts() {
+    const products = await axios.get("http://localhost:3000/api/shop/products");
+    console.log(products);
+    for (const product of products.data) {
+        const ProductDiv =  mainProductDiv.cloneNode(true);
+        const card = ProductDiv.firstElementChild.firstElementChild;
+        const imgProduct = card.firstElementChild;
+        const titleProduct = imgProduct.nextElementSibling.firstElementChild;
+        const desProduct = titleProduct.nextElementSibling;
+        const priceProduct = imgProduct.nextElementSibling.nextElementSibling.firstElementChild;
+        
+        titleProduct.innerText = product.title;
+        imgProduct.setAttribute("src",product.imageUrl);
+        priceProduct.innerText = `$${product.price}`;
+        desProduct.innerText = product.description;
+        card.setAttribute("id",product.id);
+        mainProductDiv.appendChild(card.parentElement);
+    }
+}
 
 function addToCart(e) {
     if(e.target.id==="cart"){
         if(!cart.includes(e.target.parentElement.parentElement)){
+            let price = parseFloat(e.target.parentElement.previousElementSibling.innerText.split("$")[1]);
+            console.log(price);
+            total+=price;
             cart.push(e.target.parentElement.parentElement);
-            msg="Product added to the cart"
+            msg="Product added to the cart";
+            totalDiv.innerText = 'total '+total;
         }else{
             msg="Product is already in cart"
         }
@@ -31,18 +58,18 @@ function addToCart(e) {
         cartBtn.innerHTML = `Cart <sup>${cart.length}</sup>`;
 
     }else if(e.target.className==="card-img-top"){
-        let price = parseFloat(e.target.nextElementSibling.nextElementSibling.innerText.split("$")[1]);
-        total+=price;
-        console.log(total);
-        totalDiv.innerText = 'total '+total;
-        sideDiv.style.display="inline-block";
+        
+        
         const ele = e.target.parentElement.cloneNode(true);
         ele.className = "d-flex flex-row m-2";
         sideDiv.appendChild(ele);
-        sideClosebtn.addEventListener("click",close);
+        
     }
 }
 
+function showCart(e) {
+    sideDiv.style.display="inline-block";
+}
 function close(e) {
     sideDiv.innerContent= sideDiv.firstElementChild;
     sideDiv.style.display="none";
